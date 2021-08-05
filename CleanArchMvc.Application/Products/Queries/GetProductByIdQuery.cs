65e950570e4576abf3c027f0849ad07ae1bbebb8
Jonatan Gall <jonatangd.souza@gmail.com>
2021-08-05 02:33:26 -0300

    commands and queries

---
+++ CleanArchMvc.Application/Products/Queries/GetProductByIdQuery.cs
@@ -0,0 +1,13 @@
+﻿using CleanArchMvc.Domain.Entities;
+using MediatR;
+
+namespace CleanArchMvc.Application.Products.Queries {
+    class GetProductByIdQuery : IRequest<Product> {
+
+        public int Id { get; set; }
+
+        public GetProductByIdQuery(int id) {
+            Id = id;
+        }
+    }
+}
