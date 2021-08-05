a1b07f72735b67a58b539ff7419659a963f0d4ec
Jonatan Gall <jonatangd.souza@gmail.com>
2021-08-04 22:14:04 -0300

    Domain

---
+++ CleanArchMvc.Domain/Entities/Product.cs
@@ -0,0 +1,45 @@
+using CleanArchMvc.Domain.Validation;
+
+namespace CleanArchMvc.Domain.Entities {
+    public sealed class Product: Entity {
+
+        public Product(string name, string description, decimal price, int stock, string image) {
+            ValidateDomain(name, description, price, stock, image);
+        }
+        public Product(int id, string name, string description, decimal price, int stock, string image) {
+            DomainExceptionValidation.When(id < 0, "Invalid id value");
+            ValidateDomain(name, description, price, stock, image);
+            Id = id;
+
+        }
+        public void Update(int id, string name, string description, decimal price, int stock, string image, int categoryId) {
+            ValidateDomain(name, description, price, stock, image);
+            CategoryId = categoryId;
+        }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public decimal Price { get; private set; }
+        public int Stock { get; private set; }
+        public string Image { get; private set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+        private void ValidateDomain(string name, string description, decimal price, int stock, string image) {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Invalid name.Name is required");
+            DomainExceptionValidation.When(name.Length < 3, "Invalid name.Name is too short, minimum 3 chars");
+
+            DomainExceptionValidation.When(string.IsNullOrEmpty(description), "Invalid description.Description is required");
+            DomainExceptionValidation.When(description.Length < 5, "Invalid description.Description is too short, minimum 5 chars");
+
+            DomainExceptionValidation.When(price < 0, "Invalid price value");
+            DomainExceptionValidation.When(stock < 0, "Invalid stock value");
+            DomainExceptionValidation.When(image?.Length > 250, "Invalid image name, is too short, max 250 chars");
+
+            Name = name;
+            Description = description;
+            Price = price;
+            Stock = stock;
+            Image = image;
+        }
+    }
+}
